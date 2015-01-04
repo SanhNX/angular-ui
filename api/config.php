920470c36d63e7b46906b0f0d720c2fa95c30628
SanhNX <sanh23021987@gmail.com>
2015-01-05 00:37:58 +0700

    add

--- api/config.php
+++ api/config.php
@@ -1,6 +1,18 @@
 <?php
-define('DB_USERNAME', 'root');
+/*define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'prj_angular');
+define('DB_NAME', 'prj_angular');*/
+
+$username = 'root';
+$password = '';
+$hostname = 'localhost';
+$db = 'prj_angular';
+
+$dbhandle = mysql_connect($hostname, $username, $password) or die("Unable to connect to MySQL");
+
+
+mysql_select_db($db);
+mysql_query("SET NAMES 'utf8'", $dbhandle);
+
 ?>
